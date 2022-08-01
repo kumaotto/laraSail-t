c91c48d10ba79f5700c7b942c0d4d10a10e24ee6
kumaotto <won.artoiler09@icloud.com>
2022-08-02 01:22:19 +0900

    add login/logout

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>ログイン</title>
+</head>
+
+<body>
+    @isset($errors)
+    <p style="color:red;">{{ $errors->first('message') }}</p>
+    @endisset
+    <form name="loginform" action="/login" method="POST">
+        {{ csrf_field() }}
+        <dl>
+            <dt>メールアドレス：</dt>
+            <dd>
+                <input type="text" name="email" size="30" value="{{ old('email') }}">
+            </dd>
+            <dt>パスワード：</dt>
+            <dd>
+                <input type="password" name="password" size="30">
+            </dd>
+        </dl>
+        <button type="submit" name="action" value="send">ログイン</button>
+    </form>
+</body>
+
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,16 @@
 </head>
 
 <body>
-	<p>Hello World</p>
+	<p>Hello World
+@if (Auth::check())
+	{{ \Auth::user()->name }}さん</p>
+	<p><a href="/logout">ログアウト</a></p>
+@else
+	ゲストさん</p>
+	<p><a href="/login">ログイン</a></p>
+	<br>
+	<p><a href="/register">会員登録</a></p>
+@endif
 </body>
 
 </html>
